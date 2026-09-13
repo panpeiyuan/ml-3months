7949e1230d9a6bd94423d123d0fb9f1c6666f5de
panpeiyuan <327372935+panpeiyuan@users.noreply.github.com>
2026-09-13 14:04:13 +0800

    input\if,elif,else\int(input())

--- day3.py
+++ day3.py
@@ -1,22 +1,22 @@
-password=input("password:pan")
-if password=="panpeiyuan":
-    print("Welcome")
-else:
-    print("fail to log in")
+# a=int(input())
+# b=int(input())
+# print(a+b,a-b,a/b,a//b,a%b)
 
-#转义字符
-#制表符：\t 通常表示空4个字符，也称缩进
-print('six\tsixsix')
-#\n 换行符
-print('hh\nxx')
-#\R 回车，表示将当前位置，移到本行开头
-print('123\r456')
-#\\ 反斜杠符号
-print('abc\trfj')
-print('abc\\tefj')
-print('abc\\\tefj')
-print(r'abc\\')
-print('abc\\')
-a=input()
-b=input()
-print(a+b,a-b,a/b,a//b,a%b)
+score=int(input())
+if score>=90:
+    print('A')
+elif score>=80:
+    print('B')
+elif score>=60:
+    print('C')
+elif score<60:
+    print('D')
+
+year=int(input())
+if (year%4==0 and year%100!=0) or (year%400==0):
+    print('闰年')
+else:
+    print('不是闰年')
+
+
+
